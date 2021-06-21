76d8bc244bfad49386fbab481c894f754c47477d
felippedesouza <sergiofelippe.deiro@gmail.com>
2021-06-21 10:01:59 -0300

    usando o material-ui no Carrinho de compras

--- src/components/reserva/HistoricoPedido.js
+++ src/components/reserva/HistoricoPedido.js
@@ -40,7 +40,7 @@ const HistoricoPedido = props => {
 
    return (
       <TableContainer component={Paper}>
-         <Table aria-label="collapsible table">
+         <Table stickyHeader aria-label="collapsible table">
             <TableHead>
                <TableRow>
                   <TableCell />
@@ -55,11 +55,11 @@ const HistoricoPedido = props => {
                   reservas = props.reservas.filter(reserva => reserva.numero_pedido === pedido.numero_pedido)
 
                   return (
-                     <Linha key={pedido.numero_pedido} 
-                           pedido={pedido} 
-                           produtos={props.produtos} 
-                           id_usuario={id_usuario} 
-                           reservas={reservas} />
+                     <Linha key={pedido.numero_pedido}
+                        pedido={pedido}
+                        produtos={props.produtos}
+                        id_usuario={id_usuario}
+                        reservas={reservas} />
                   )
                })}
             </TableBody>
@@ -82,14 +82,22 @@ function Linha(props) {
 
    const formatar_data = data => {
       data = new Date(data)
-      return `${data.getDay()}/${data.getMonth()}/${data.getFullYear()} ${data.getHours()}:${data.getMinutes()}:${data.getSeconds()}`
+      
+      const dia = data.getDay().toString()
+      const mes = data.getMonth() + 1
+      const ano = data.getFullYear()
+      const hora = data.getHours()
+      const minuto = data.getMinutes()
+      const segundo = data.getSeconds()
+
+      return `${dia}/${mes}/${ano} ${hora}:${minuto}:${segundo}`
    }
 
    const { produtos, pedido } = props
 
    let reservas = props.reservas
 
-   function montar_tabela(){
+   function montar_tabela() {
       let qtd_itens = 0
       let produto, sub_total
 
@@ -99,7 +107,7 @@ function Linha(props) {
          if (produto === undefined) {
             return (<></>)
          }
-         
+
          if (reserva.numero_pedido !== pedido.numero_pedido) {
             return (<></>)
          }
@@ -121,43 +129,43 @@ function Linha(props) {
 
       return (
          <>
-         <TableRow className={classes.root}>
-            <TableCell>
-               <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
-                  {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-               </IconButton>
-            </TableCell>
-            <TableCell component="th" scope="row">
-               {qtd_itens}
-            </TableCell>
-            <TableCell align="right">{formatar_valor(pedido.total)}</TableCell>
-            <TableCell align="right">{formatar_data(pedido.data_pedido)}</TableCell>
-            <TableCell align="right">{pedido.numero_pedido}</TableCell>
-         </TableRow>
-         <TableRow>
-            <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-               <Collapse in={open} timeout="auto" unmountOnExit>
-                  <Box margin={1}>
-                     <Typography variant="h6" gutterBottom component="div">
-                        Reserva
-            </Typography>
-                     <Table size="small" aria-label="purchases">
-                        <TableHead>
-                           <TableRow>
-                              <TableCell>Nome</TableCell>
-                              <TableCell>Preco</TableCell>
-                              <TableCell align="right">Quantidade</TableCell>
-                              <TableCell align="right">Subtotal</TableCell>
-                           </TableRow>
-                        </TableHead>
-                        <TableBody>
-                           {linha_tabela_reserva}
-                        </TableBody>
-                     </Table>
-                  </Box>
-               </Collapse>
-            </TableCell>
-         </TableRow>
+            <TableRow className={classes.root}>
+               <TableCell>
+                  <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
+                     {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                  </IconButton>
+               </TableCell>
+               <TableCell component="th" scope="row">
+                  {qtd_itens}
+               </TableCell>
+               <TableCell align="right">{formatar_valor(pedido.total)}</TableCell>
+               <TableCell align="right">{formatar_data(pedido.data_pedido)}</TableCell>
+               <TableCell align="right">{pedido.numero_pedido}</TableCell>
+            </TableRow>
+            <TableRow>
+               <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+                  <Collapse in={open} timeout="auto" unmountOnExit>
+                     <Box margin={1}>
+                        <Typography variant="h6" gutterBottom component="div">
+                           Reserva
+                        </Typography>
+                        <Table size="small" aria-label="purchases">
+                           <TableHead>
+                              <TableRow>
+                                 <TableCell>Nome</TableCell>
+                                 <TableCell>Preco</TableCell>
+                                 <TableCell align="right">Quantidade</TableCell>
+                                 <TableCell align="right">Subtotal</TableCell>
+                              </TableRow>
+                           </TableHead>
+                           <TableBody>
+                              {linha_tabela_reserva}
+                           </TableBody>
+                        </Table>
+                     </Box>
+                  </Collapse>
+               </TableCell>
+            </TableRow>
          </>
       )
    }
